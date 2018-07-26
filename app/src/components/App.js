a9ff74d4da7e104e2a6814726c68d090ba7326ea
m18770074875_1@163.com <m18770074875_1@163.com>
2018-07-26 22:55:18 +0800

    hb

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 import Footer from "./Footer";
+import HongBao from "./HongBao";
 	
 const App = () => (
 	<div>	
 		<AddTodo></AddTodo>
 		<VisibleTodoList></VisibleTodoList>
 		<Footer></Footer>
+		<HongBao></HongBao>
 	</div>
 )
 
